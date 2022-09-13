d251560a9a9f053f840be370ae519f972fe1590b
wangyiyan <wang-yiyan@qq.com>
2022-09-14 02:52:02 +0800

    feat: display local time.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [date, setDate] = React.useState(new Date());
+
+  //Replaces componentDidMount and componentWillUnmount
+  React.useEffect(() => {
+    var timerID = setInterval(() => tick(), 1000);
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  });
+
+  function tick() {
+    setDate(new Date());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            {date.toLocaleString("sv-SE")}
+          </p>
+        </header>
+      </div>
   );
 }
 
